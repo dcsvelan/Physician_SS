5915444ce638423b9ced54e5d8131eb374ea2e19
dcs velan <dcsvelan@gmail.com>
2025-07-31 14:06:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
 
 # Create LLM Object using the new model
 llm = ChatGroq(
-    model="mistral-saba-24b", 
+    model="Llama 3.1 8B Instant 128k", 
     temperature=0
 )
 
